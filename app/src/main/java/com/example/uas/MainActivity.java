21a9eb6e8831fb3f9c65f62947e0b1b1ffad241a
seosain <wowhusain17@gmail.com>
2019-05-29 14:39:49 +0700

    UAS ANDROID

---
+++ app/src/main/java/com/example/uas/MainActivity.java
@@ -0,0 +1,110 @@
+package com.android.uas;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import com.android.cinematix.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner spClass;
+    // TODO (5) Buatlah variabel private untuk komponen UI berikut :
+    // Spinner spClass (spMovie), EditText (etJumlah),
+    // RadioGroup (rgAdditional), RadioButton (rbAdditional), Button (btnBook)
+    // [ikuti contoh di atas (spClass)]
+
+    private List<String> classCinematix, movieCinematix;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Inisialisasi komponen berdasarkan id dari layout ke dalam variabel
+        spClass = (Spinner) findViewById(R.id.sp_class);
+        // TODO (6) Lanjutkan inisialisasi value untuk "Spinner (spMovie)"
+        // [ikuti contoh di atas (spClass)]
+
+        // TODO (7) Lanjutkan inisialisasi value untuk "EditText (etJumlah)"
+
+        // TODO (8) Lanjutkan inisialisasi value untuk "RadioGroup (rgAdditional)"
+
+        // TODO (9) Lanjutkan inisialisasi value untuk "Button (btnBook)"
+
+        // Berikan spinner beberapa data
+        classCinematix = new ArrayList<String>();
+        classCinematix.add("Ekonomi");
+        classCinematix.add("Regular");
+        classCinematix.add("Eksekutif");
+
+        movieCinematix = new ArrayList<String>();
+        movieCinematix.add("Emoji");
+        movieCinematix.add("Star Wars: The last Jedi");
+        movieCinematix.add("Rumah Pengabdi Setan?");
+        movieCinematix.add("Interstellar");
+        movieCinematix.add("Blade Runner 2049");
+        movieCinematix.add("Thor: Ragnarok");
+        movieCinematix.add("Pokemon: I Choose You");
+        movieCinematix.add("Happy Death Day");
+        movieCinematix.add("IT!");
+        movieCinematix.add("Guardians Of Galaxy. Volume 2");
+
+        // Buatlah adapter untuk spinner
+        ArrayAdapter<String> classCinematixAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, classCinematix);
+        // TODO (10) Lanjutkan membuat adapter untuk "movieCinematixAdapter"
+        // [ikuti contoh di atas (classCinematixAdapter)]
+
+        // Set adapter tadi ke dalam spinner
+        spClass.setAdapter(classCinematixAdapter);
+        // TODO (11) Lanjutkan men-setup "spMovie"
+        // [ikuti contoh di atas (spClass)]
+
+        // Deteksi kapan tombol "Pesan Tiket" di tekan
+        btn_book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                boolean valid = true;
+
+                if (etJumlah.getText().length() < 1) {
+                    // TODO (12.A) setError pada "etJumlah" dengan pernyataan "Wajib diisi!"
+                    valid = false;
+                } else if (Integer.parseInt(etJumlah.getText().toString()) < 1) {
+                    // TODO (12.B) setError pada "etJumlah" dengan pernyataan "Minimal 1 tiket!"
+                    valid = false;
+                }
+
+                // Jika valid, lanjutkan ke ResultActivity
+                if (valid) {
+                    int selectedId = rgAdditional.getCheckedRadioButtonId();
+                    rbAdditional = (RadioButton) findViewById(selectedId);
+
+                    // TODO (13.A) Buatlah object "Intent" dengan sumber "MainActivity", dan tujuannya ke "ResultActivity"
+
+                    // Set data untuk dikirimkan ke ResultActivity
+                    intent.putExtra("classCinematix", spClass.getSelectedItem().toString());
+                    // TODO (13.B) Masukkan "spMovie" ke dalam "intent" dengan "putExtra" menggunakan konstanta "movieCinematix"
+                    // [ikuti contoh di atas (spClass)]
+
+                    intent.putExtra("additionalCinematix", rbAdditional.getText().toString());
+                    // TODO (13.B) Masukkan "etJumlah" ke dalam "intent" dengan "putExtra" menggunakan konstanta "jumlahCinematix"
+                    // [ikuti contoh di atas (rbAdditional)]
+
+                    // Pindah activity ke ResultActivity
+                    // TODO (13.C) Gunakan method "startActivity()" dengan parameter "intent"
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/uas/ResultActivity.java
@@ -0,0 +1,133 @@
+package com.android.uas;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.cinematix.R;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class ResultActivity extends AppCompatActivity {
+
+    private TextView tvClass, tvMovie, tvRate, tvJumlah, tvAdditional;
+    private TextView tvSubTotalTicket, tvSubTotalAddtional, tvTotal;
+    private Button btnShare;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        // Inisialisasi komponen berdasarkan id dari layout ke dalam variabel
+        tvClass = (TextView) findViewById(R.id.tv_class);
+        tvMovie = (TextView) findViewById(R.id.tv_movie);
+        tvRate = (TextView) findViewById(R.id.tv_rate);
+        tvJumlah = (TextView) findViewById(R.id.tv_jumlah);
+        tvAdditional = (TextView) findViewById(R.id.tv_additional);
+
+        tvSubTotalTicket = (TextView) findViewById(R.id.tv_ticket_subtotal);
+        tvSubTotalAddtional = (TextView) findViewById(R.id.tv_additional_subtotal);
+        tvTotal = (TextView) findViewById(R.id.tv_total);
+
+        btnShare = (Button) findViewById(R.id.btn_share);
+
+        // Deklarasikan intent
+        Intent intent = getIntent();
+
+        // Ambil data yang dikirimkan dari MainActivity
+        final String classCinematix = intent.getStringExtra("classCinematix");
+        tvClass.setText(classCinematix);
+
+        // TODO (14) Buatlah variabel "movieCinematix" untuk menampung data yang dikirimkan dari "MainActivity"
+        // [ikuti contoh di atas (classCinematix)]
+
+        // TODO (15) Gunakan method "getPriceBasedOnClass()" dengan parameter "classCinematix" ke dalam variabel "rate"
+        int rate = ;
+        tvRate.setText(convertMoney(rate));
+
+        final String jumlah = intent.getStringExtra("jumlahCinematix");
+        tvJumlah.setText(jumlah);
+
+        final String additional = intent.getStringExtra("additionalCinematix");
+        int priceAdditional = getPriceBasedOnAdditional(additional);
+        // TODO (16) Gunakan method "convertMoney()" dengan parameter "priceAdditional" setelah tanda "+"
+        tvAdditional.setText(additional + " : " + );
+
+        // Hitung subtotal
+        int priceClass = getPriceBasedOnClass(classCinematix);
+
+        // TODO (17.A) Ubahlah tipe data variabel "jumlah" dengan menggunakan method "Integer.valueOf()"
+        int subTotalMovie = priceClass * jumlah;
+        tvSubTotalTicket.setText(convertMoney(subTotalMovie));
+
+        // TODO (17.A) Ubahlah tipe data variabel "jumlah" dengan menggunakan method "Integer.valueOf()"
+        int subTotalAddtional = priceAdditional * jumlah;
+        tvSubTotalAddtional.setText(convertMoney(subTotalAddtional));
+
+        // Hitung total
+        final int total = subTotalMovie + subTotalAddtional;
+        tvTotal.setText(convertMoney(total));
+
+        // Deteksi kapan tombol "Bagikan Ke Teman" di tekan
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sendText = "Memesan " + jumlah + " tiket " +
+                        movieCinematix + ". Kelas " + classCinematix +
+                        ", include popcorn dan soda : " + additional + ".\n" +
+                        "Total Harga : " + convertMoney(total);
+
+                // TODO (18.A) Deklarasikan object "Intent" dengan nama "sendIntent"
+
+                // TODO (18.B) Set tipe "sendIntent" menjadi "text/plain"
+                // TODO (18.C) Set action "sendIntent" menjadi "Intent.ACTION_SEND"
+                sendIntent.putExtra(Intent.EXTRA_TEXT, sendText);
+
+                // Pindah activity ke activity bawaan android untuk share data
+                startActivity(Intent.createChooser(sendIntent, "Bagikan"));
+            }
+        });
+    }
+
+    public int getPriceBasedOnClass(String classCinema) {
+        int price = 0;
+        switch (classCinema) {
+            case "Ekonomi":
+                price = 20000;
+                break;
+            case "Regular":
+                price = 30000;
+                break;
+            case "Eksekutif":
+                price = 50000;
+                break;
+            default:
+                price = 0;
+                break;
+        }
+        return price;
+    }
+
+    public int getPriceBasedOnAdditional(String additional) {
+        int price = 0;
+        switch (additional) {
+            case "Ya":
+                price = 20000;
+                break;
+            case "Tidak":
+            default:
+                price = 0;
+                break;
+        }
+        return price;
+    }
+
+    public String convertMoney(int money) {
+        return NumberFormat.getNumberInstance(Locale.GERMANY).format(money);
+    }
+}
